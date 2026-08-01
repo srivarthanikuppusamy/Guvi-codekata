10ffed008801300dc19d5329218554c386d13789
SRIVARTHANI KUPPUSAMY <srivarthanikuppusamy@gmail.com>
2026-08-01 11:36:20 +0530

    problem - 3

---
+++ 3. Check Divisibility by 7.py
@@ -0,0 +1,20 @@
+# Problem Statement:
+# Given a number N, print yes if the number is a multiple of 7 else print no.
+
+# Input Description:
+# The input consists of a single integer N.
+
+# Output Description:
+# Print 'yes' if N is a multiple of 7, otherwise print 'no'.
+
+# Sample Input:
+# 49
+
+# Sample Output:
+# yes
+
+n= int(input())
+if n%7==0:
+    print("yes")
+else:
+    print("no")
